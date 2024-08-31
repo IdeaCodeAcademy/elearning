57a725460d0f2c5ef2577670bf8a411f12b7a25b
Agga <agga@Aggas-MacBook-Air.local>
2024-08-31 11:42:14 +0700

    add

--- ica_elearning/models/ica_course.py
+++ ica_elearning/models/ica_course.py
@@ -15,8 +15,20 @@ class IcaCourse(models.Model):
     author_ids = fields.Many2many('res.partner', string='Authors')
     module_ids = fields.One2many('ica.course.module', 'course_id', string='Modules',
                                  domain=[('state', '=', 'published')])
+    module_count = fields.Integer(string='Modules', compute="_compute_module_count")
     lesson_ids = fields.One2many('ica.course.lesson', 'course_id', string='Lessons',
                                  domain=[('state', '=', 'published')])
+    lesson_count = fields.Integer(string='Lessons', compute="_compute_lesson_count")
+
+    @api.depends('lesson_ids')
+    def _compute_lesson_count(self):
+        for rec in self:
+            rec.lesson_count = len(rec.lesson_ids)
+
+    @api.depends('module_ids')
+    def _compute_module_count(self):
+        for rec in self:
+            rec.module_count = len(rec.module_ids)
 
     def action_draft(self):
         self.state = 'draft'
@@ -26,3 +38,22 @@ class IcaCourse(models.Model):
 
     def action_unpublished(self):
         self.state = 'unpublished'
+
+    def action_view_modules(self):
+        return {
+            "name": f"{self.name}'s Modules",
+            "type": "ir.actions.act_window",
+            "res_model": "ica.course.module",
+            "view_mode": "tree,form",
+            "domain": [("course_id", "=", self.id)],
+            "context": {"default_course_id": self.id},
+        }
+
+    def action_view_lessons(self):
+        return {
+            "name": f"{self.name}'s Lessons",
+            "type": "ir.actions.act_window",
+            "res_model": "ica.course.lesson",
+            "view_mode": "tree,form",
+            "domain": [("course_id", "=", self.id)],
+        }
