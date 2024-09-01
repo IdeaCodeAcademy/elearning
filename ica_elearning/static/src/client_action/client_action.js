ad7798094c25f730a9b0d1eef085cab464f8bf2f
Agga <agga@Aggas-MacBook-Air.local>
2024-09-01 13:58:48 +0700

    add client action

--- ica_elearning/static/src/client_action/client_action.js
+++ ica_elearning/static/src/client_action/client_action.js
@@ -1,10 +1,26 @@
 /** @odoo-module **/
 
-import { registry } from "@web/core/registry";
+import {registry} from "@web/core/registry";
 
-import { Component } from  "@odoo/owl";
+import {Component,useState,onWillStart} from "@odoo/owl";
 
-class IcaCourseClientAction extends Component {}
+class IcaCourseClientAction extends Component {
+    setup() {
+        this.state = useState({
+            courses: []
+        })
+        this.ormService = this.env.services.orm;
+        onWillStart(async () => {
+            await this.getAllPublishedCourses();
+        })
+    }
+
+    async getAllPublishedCourses() {
+        this.state.courses = await this.ormService.searchRead('ica.course', [['state', '=', 'published']], ['id', 'name', 'state','fees'], {limit: 5})
+    }
+}
+
+
 IcaCourseClientAction.template = "ica_elearning.IcaCourseClientAction";
 
 // remember the tag name we put in the first step
