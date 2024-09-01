75e7c9a6e1df13d607e4a35497ec66a8e3aa905b
Agga <agga@Aggas-MacBook-Air.local>
2024-09-01 12:26:04 +0700

    add

--- ica_elearning/models/ica_course_enrollment.py
+++ ica_elearning/models/ica_course_enrollment.py
@@ -8,5 +8,9 @@ class IcaCourseEnrollment(models.Model):
 
     partner_id = fields.Many2one('res.partner')
     currency_id = fields.Many2one('res.currency', string='Currency', default=lambda self: self.env.company.currency_id)
-    total_amount = fields.Monetary(currency_field='currency_id')
+    total_amount = fields.Monetary(currency_field='currency_id',compute="_compute_total_amount")
     line_ids = fields.One2many('ica.course.enrollment.line', 'enrollment_id')
+
+    @api.depends('line_ids')
+    def _compute_total_amount(self):
+        self.total_amount = sum(self.line_ids.mapped('fees'))
