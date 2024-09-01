f404b02b89d1e6fbe58f1c2b3d107612770d01c7
Agga <agga@Aggas-MacBook-Air.local>
2024-09-01 19:09:42 +0700

    add render template

--- ica_elearning/controller/ica_course_controller.py
+++ ica_elearning/controller/ica_course_controller.py
@@ -2,6 +2,7 @@ from odoo.http import Controller, route, request
 
 
 class IcaCourseController(Controller):
-    @route('/ica/home', auth="public", type="http",website=True)
+    @route('/ica/home', auth="public", type="http", website=True)
     def index(self):
-        return request.render('ica_elearning.home', {})
+        course_ids = request.env['ica.course'].search([('state', '=', 'published')])
+        return request.render('ica_elearning.home', {'course_ids': course_ids})
